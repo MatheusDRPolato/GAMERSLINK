4890ccc721076418454f562571b39acb640a8863
Matheus Dos Reis Polato <99278233+MatheusDRPolato@users.noreply.github.com>
2023-08-03 06:36:10 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+    <link rel="stylesheet" href="/css/menu.css" type="text/css"/>  
+    <link rel="icon" href="/imagens/OIG.jpg" type="image/x-icon">
+</head>
+<body>
+    <div id="Barra_Nav">     
+            <img id="logo" src="imagens/avipam.png">
+            <div class="icons">
+                <a href="menu.HTML">
+                    <img src="/imagens/casaicon.png"  alt="menu"></a>
+                <img src="imagens/comentarioicon.png">
+                <img src="imagens/notificacaoicon.png">
+                <a href="perfil.html">
+                    <img src="/imagens/pessoaicon.png"  alt="pessoa"></a>
+            </div>
+        <div id="Pesquisar">
+            <input type="text" id="txtBusca" placeholder="Digite"/>
+            <img src="imagens/iconepesquisa.png" id="btnBusca" alt="Buscar"/>
+          </div>
+    </div>
+
+
+    <div id="Rodapé"><h1>oi</h1></div>
+    <script src="/js/menu1.js">
+    </script>
+</body>
+</html>
